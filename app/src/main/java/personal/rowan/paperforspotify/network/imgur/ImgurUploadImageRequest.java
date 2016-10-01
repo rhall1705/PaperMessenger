9592927f088200a019b33d39b7ff4b9006b88e17
rowanhall <rowan@yikyakapp.com>
2016-10-01 15:04:27 -0400

    Client code with censored network package

---
+++ app/src/main/java/personal/rowan/paperforspotify/network/imgur/ImgurUploadImageRequest.java
@@ -0,0 +1,63 @@
+package personal.rowan.paperforspotify.network.imgur;
+
+import android.net.Uri;
+
+import java.io.File;
+
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import personal.rowan.paperforspotify.network.BaseRequest;
+
+public class ImgurUploadImageRequest
+		extends BaseRequest<ImgurUploadImageResponse> {
+
+	private static final String CLIENT_ID = "YOUR_CLIENT_ID_HERE";
+	private static final String SCHEME = "https";
+	private static final String BASE_URL = "api.imgur.com";
+	private static final String VERSION_NUMBER = "3";
+	private static final String PATH = "image";
+	private static final String AUTH = "Client-ID " + CLIENT_ID;
+
+	private String mTitle;
+	private String mDescription;
+	private File mFile;
+
+	public ImgurUploadImageRequest(String title, String description,  File file) {
+		mTitle = title;
+		mDescription = description;
+		mFile = file;
+	}
+
+	@Override
+	protected ImgurUploadImageResponse buildResponse(String responseString, Exception e) {
+		return new ImgurUploadImageResponse(responseString, e);
+	}
+
+	@Override
+	protected RequestBody buildRequestBody() {
+		return RequestBody.create(null, mFile);
+	}
+
+	@Override
+	protected String getUrl() {
+		return new Uri.Builder()
+				.scheme(SCHEME)
+				.authority(BASE_URL)
+				.appendPath(VERSION_NUMBER)
+				.appendPath(PATH)
+				.appendQueryParameter("title", mTitle)
+				.appendQueryParameter("description", mDescription)
+				.build().toString();
+	}
+
+	@Override
+	protected MethodType getMethodType() {
+		return MethodType.POST;
+	}
+
+	@Override
+	protected void addHeaders(Request.Builder builder) {
+		builder.addHeader("Authorization", AUTH);
+	}
+
+}
